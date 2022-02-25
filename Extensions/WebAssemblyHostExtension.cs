769d1e4115a396a60f270a7e85bab9c3172300f7
Rizo11 <20455251@fhda.edu>
2022-02-26 02:14:52 +0500

    resx added

---
+++ Extensions/WebAssemblyHostExtension.cs
@@ -0,0 +1,30 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+
+namespace akiss.Extensions
+{
+    public static class WebAssemblyHostExtension
+    {
+        public async static Task SetDefaultCulture(this WebAssemblyHost host)
+        {
+            var localStorage = host.Services.GetRequiredService<ILocalStorageService>();
+            var fromLs = await localStorage.GetItemAsync<string>("culture");
+
+            CultureInfo culture;
+
+            if(fromLs != null)
+            {
+                culture = new CultureInfo(fromLs);
+            }
+            else
+            {
+                culture = new CultureInfo("en-US");
+            }
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture; 
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using akiss;
+using Blazored.LocalStorage;
+using akiss.Extensions;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddLocalization();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+var host = builder.Build();
 
-await builder.Build().RunAsync();
+await host.SetDefaultCulture();
+await host.RunAsync();
