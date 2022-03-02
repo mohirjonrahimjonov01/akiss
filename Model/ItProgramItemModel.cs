ddd1f80b87bb8db073feb1656861f5222d916be3
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2022-03-02 17:49:58 +0500

    Some changes

---
+++ Model/ItProgramItemModel.cs
@@ -0,0 +1,12 @@
+namespace akiss.Models;
+
+public class ItProgramItemModel
+{
+    public string Name { get; set; }
+    
+    public string Tag { get; set; }
+    
+    public string Description { get; set; }
+    public string Image { get; set; }
+    
+}
