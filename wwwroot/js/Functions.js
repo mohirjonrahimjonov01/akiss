e6257c44be1c7559a62dd56451d94d58f9038bce
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2022-03-05 13:49:13 +0500

    formalar tuzatildi Admission form Script ishladi

--- wwwroot/js/Functions.js
+++ wwwroot/js/Functions.js
@@ -1,7 +1,7 @@
 function Send_Data(){
-    const scriptURL = 'https://script.google.com/macros/s/AKfycby_HCrx8RTyylqfgb3L20296hmQWpZ-qNigjkYXgHWO9QKd8TO_IsqUyRa1MqpMNQTH/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxvVVtST9fMRFuNOOFKhp0ToSnjdPYnyUMrH-qsCvw_bJxwZV4m1OnTOLBXiOodKrY4/exec'
     const form = document.forms['contact-form']
-    form.addEventListener('submit', e => {
+    form.addEventListener('submit',e => {
       e.preventDefault()
     });
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
@@ -11,4 +11,19 @@ function Send_Data(){
         document.getElementById("res_suc").setAttribute("hidden","hidden");
         document.getElementById("res_err").setAttribute("hidden","hidden");
       }, 4500))
+}
+
+function Send_Admission_Data(){
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxvVVtST9fMRFuNOOFKhp0ToSnjdPYnyUMrH-qsCvw_bJxwZV4m1OnTOLBXiOodKrY4/exec'
+  const form = document.forms['admission-form']
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+  });
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => document.getElementById("res_success").removeAttribute("hidden"))
+    .catch(error => document.getElementById("res_error").removeAttribute("hidden"))
+    .finally(setTimeout(function(){
+      document.getElementById("res_success").setAttribute("hidden","hidden");
+      document.getElementById("res_error").setAttribute("hidden","hidden");
+    }, 4500))
 }
