239a1515ec2fe5c16ef631d35a341ffdd6ebf2be
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2022-03-07 23:41:01 +0500

    apscript almost done

--- wwwroot/js/Functions.js
+++ wwwroot/js/Functions.js
@@ -14,7 +14,7 @@ function Send_Data(){
 }
 
 function Send_Admission_Data(){
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbxvVVtST9fMRFuNOOFKhp0ToSnjdPYnyUMrH-qsCvw_bJxwZV4m1OnTOLBXiOodKrY4/exec'
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz4ojYoPN3RCIKv-Z0Y4Ph7xV9B1ZTAx6Qp-aRRkl21ZcbquaAhwfyGuiX-PmGlx2Iy/exec'
   const form = document.forms['admission-form']
   form.addEventListener('submit', e => {
     e.preventDefault()
@@ -25,5 +25,20 @@ function Send_Admission_Data(){
     .finally(setTimeout(function(){
       document.getElementById("res_success").setAttribute("hidden","hidden");
       document.getElementById("res_error").setAttribute("hidden","hidden");
+    }, 4500))
+}
+
+function Send_Team_Data(){
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbziWrCrohkUSA5a_2r3pp9xFeELLFQq4fk_ROlCt2vd2MlpwdinlOuwRxrvK6Df3dv-ig/exec'
+  const form = document.forms['teacher-form']
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+  });
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => document.getElementById("res_suc").removeAttribute("hidden"))
+    .catch(error => document.getElementById("res_err").removeAttribute("hidden"))
+    .finally(setTimeout(function(){
+      document.getElementById("res_suc").setAttribute("hidden","hidden");
+      document.getElementById("res_err").setAttribute("hidden","hidden");
     }, 4500))
 }
