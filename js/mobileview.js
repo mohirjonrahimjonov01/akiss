9aa586d7d1393505293b01deb7e69e2576e4095c
shahbozkhurramov <89694175+shahbozkhurramov@users.noreply.github.com>
2022-03-16 17:56:47 +0000

    Deploying to gh-pages from @ ilmhub-uz/akiss@7e953a6b15dbe4046240bf5dbb1fae17b1eafc6a 🚀

--- js/mobileview.js
+++ js/mobileview.js
@@ -1,37 +1,230 @@
-function changeVisibilityOfCategories() {
-    var devicewidth = document.documentElement.clientWidth;
-    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+"use strict";
 
-    var element = document.getElementById("navbar");
-    var navbarburger = document.getElementById("navbar-burger");
-    if(devicewidth < 991 || width < 991) {
-        if(!element.classList.contains("navbar-mobile"))
-        {
-            element.classList.add("navbar-mobile");
+!function () {
+
+    window.Element.prototype.removeClass = function () {
+        let className = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "",
+            selectors = this;
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
         }
+        if (this.isVariableDefined(selectors) && className) {
+            selectors.classList.remove(className);
+        }
+        return this;
+    }, window.Element.prototype.addClass = function () {
+        let className = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "",
+            selectors = this;
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (this.isVariableDefined(selectors) && className) {
+            selectors.classList.add(className);
+        }
+        return this;
+    }, window.Element.prototype.toggleClass = function () {
+        let className = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "",
+            selectors = this;
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (this.isVariableDefined(selectors) && className) {
+            selectors.classList.toggle(className);
+        }
+        return this;
+    }, window.Element.prototype.isVariableDefined = function () {
+        return !!this && typeof (this) != 'undefined' && this != null;
+    }
+}();
+
+// Get CSS var value
+var ThemeColor = function () {
+  return {
+    getCssVariableValue: function (e) {
+      var t = getComputedStyle(document.documentElement).getPropertyValue(e);
+      return t && t.length > 0 && (t = t.trim()), t;
+    }
+  };
+}();
+
+var e = {
+    init: function () {
+        e.preLoader(),
+        e.megaMenu(),
+        e.stickyHeader(),
+        e.tinySlider(),
+        e.stickyBar(),
+        e.toolTipFunc(),
+        e.popOverFunc(),
+        e.backTotop(),
+        e.lightBox(),
+        e.enableIsotope(),
+        e.choicesSelect(),
+        e.aosFunc(),
+        e.dashboardChart(),
+        e.earningChart(),
+        e.earningChart2(),
+        e.trafficChart(),
+        e.activeChart(),
+        e.activeChart2(),
+        e.reviewChart(),
+        e.quill(),
+        e.stepper(),
+        e.videoPlyr(),
+        e.darkMode(),
+        e.pricing(),
+        e.stickyElement(),
+        e.overlayScrollbars();
         
-        if(!navbarburger.classList.contains("bi-x"))
-        {
-            navbarburger.classList.add("bi-x");
+    },
+    isVariableDefined: function (el) {
+        return typeof !!el && (el) != 'undefined' && el != null;
+    },
+    getParents: function (el, selector, filter) {
+        const result = [];
+        const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+        // match start from parent
+        el = el.parentElement;
+        while (el && !matchesSelector.call(el, selector)) {
+            if (!filter) {
+                if (selector) {
+                    if (matchesSelector.call(el, selector)) {
+                        return result.push(el);
+                    }
+                } else {
+                    result.push(el);
+                }
+            } else {
+                if (matchesSelector.call(el, filter)) {
+                    result.push(el);
+                }
+            }
+            el = el.parentElement;
+            if (e.isVariableDefined(el)) {
+                if (matchesSelector.call(el, selector)) {
+                    return el;
+                }
+            }
+
         }
-        if(navbarburger.classList.contains("bi-list"))
-        {
-            navbarburger.classList.remove("bi-list");
+        return result;
+    },
+    getNextSiblings: function (el, selector, filter) {
+        let sibs = [];
+        let nextElem = el.parentNode.firstChild;
+        const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+        do {
+            if (nextElem.nodeType === 3) continue; // ignore text nodes
+            if (nextElem === el) continue; // ignore elem of target
+            if (nextElem === el.nextElementSibling) {
+                if ((!filter || filter(el))) {
+                    if (selector) {
+                        if (matchesSelector.call(nextElem, selector)) {
+                            return nextElem;
+                        }
+                    } else {
+                        sibs.push(nextElem);
+                    }
+                    el = nextElem;
+
+                }
+            }
+        } while (nextElem = nextElem.nextSibling)
+        return sibs;
+    },
+    on: function (selectors, type, listener) {
+        document.addEventListener("DOMContentLoaded", () => {
+            if (!(selectors instanceof HTMLElement) && selectors !== null) {
+                selectors = document.querySelector(selectors);
+            }
+            selectors.addEventListener(type, listener);
+        });
+    },
+    onAll: function (selectors, type, listener) {
+        document.addEventListener("DOMContentLoaded", () => {
+            document.querySelectorAll(selectors).forEach((element) => {
+                if (type.indexOf(',') > -1) {
+                    let types = type.split(',');
+                    types.forEach((type) => {
+                        element.addEventListener(type, listener);
+                    });
+                } else {
+                    element.addEventListener(type, listener);
+                }
+
+
+            });
+        });
+    },
+    removeClass: function (selectors, className) {
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (e.isVariableDefined(selectors)) {
+            selectors.removeClass(className);
+        }
+    },
+    removeAllClass: function (selectors, className) {
+        if (e.isVariableDefined(selectors) && (selectors instanceof HTMLElement)) {
+            document.querySelectorAll(selectors).forEach((element) => {
+                element.removeClass(className);
+            });
+        }
+
+    },
+    toggleClass: function (selectors, className) {
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (e.isVariableDefined(selectors)) {
+            selectors.toggleClass(className);
+        }
+    },
+    toggleAllClass: function (selectors, className) {
+        if (e.isVariableDefined(selectors)  && (selectors instanceof HTMLElement)) {
+            document.querySelectorAll(selectors).forEach((element) => {
+                element.toggleClass(className);
+            });
+        }
+    },
+    addClass: function (selectors, className) {
+        if (!(selectors instanceof HTMLElement) && selectors !== null) {
+            selectors = document.querySelector(selectors);
+        }
+        if (e.isVariableDefined(selectors)) {
+            selectors.addClass(className);
+        }
+    },
+    select: function (selectors) {
+        return document.querySelector(selectors);
+    },
+    selectAll: function (selectors) {
+        return document.querySelectorAll(selectors);
+    },
+
+    // START: 03 Sticky Header
+    stickyHeader: function () {
+        var stickyNav = e.select('.navbar-sticky');
+        if (e.isVariableDefined(stickyNav)) {
+            var stickyHeight = stickyNav.offsetHeight;
+            stickyNav.insertAdjacentHTML('afterend', '<div id="sticky-space"></div>');
+            var stickySpace = e.select('#sticky-space');
+            if (e.isVariableDefined(stickySpace)) {
+                document.addEventListener('scroll', function (event) {
+                    var scTop = window.pageYOffset || document.documentElement.scrollTop;
+                    if (scTop >= 400) {
+                        stickySpace.addClass('active');
+                        e.select("#sticky-space.active").style.height = stickyHeight + 'px';
+                        stickyNav.addClass('navbar-sticky-on');
+                    } else {
+                        stickySpace.removeClass('active');
+                        stickySpace.style.height = '0px';
+                        stickyNav.removeClass("navbar-sticky-on");
+                    }
+                });
+            }
         }
     }
-    else if(devicewidth >= 991 || width >= 991) {
-        if(element.classList.contains("navbar-mobile"))
-        {
-            element.classList.remove("navbar-mobile");
-        }
-        
-        if(navbarburger.classList.contains("bi-x"))
-        {
-            navbarburger.classList.remove("bi-x");
-        }
-        if(!navbarburger.classList.contains("bi-list"))
-        {
-            navbarburger.classList.add("bi-list");
-        }
-    }
-}
+};
+e.init();
