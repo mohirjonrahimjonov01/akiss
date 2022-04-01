be53814a3704977cf1f09b7899b9918dd4c06efe
Furqat-DevDo <89734235+Furqat-DevDo@users.noreply.github.com>
2022-04-01 16:46:54 +0000

    Deploying to gh-pages from @ ilmhub-uz/akiss@2fedcf965253ae29679127184197daba3689b878 🚀

--- js/Functions.js
+++ js/Functions.js
@@ -13,10 +13,13 @@ function Send_Data(){
     setTimeout(function () {
       document.getElementById("compobtn").removeAttribute('disabled');
     }, 3000);
-    document.getElementById("contact-form").reset();
+ 
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => document.getElementById("res_suc").removeAttribute("hidden"))
       .catch(error => document.getElementById("res_err").removeAttribute("hidden"))
+      .then(document.getElementById("contact-form").reset())
+      .then( document.getElementById("name").classList.remove('is-valid'))
+      .then( document.getElementById("email").classList.remove('is-valid'))
       .finally(setTimeout(function(){
         document.getElementById("res_suc").setAttribute("hidden","hidden");
         document.getElementById("res_err").setAttribute("hidden","hidden");
@@ -33,10 +36,14 @@ function Send_Admission_Data(){
   setTimeout(function () {
     document.getElementById("adbtn").removeAttribute('disabled');
   }, 3000);
-  document.getElementById("admission-form").reset();
+
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => document.getElementById("res_success").removeAttribute("hidden"))
     .catch(error => document.getElementById("res_error").removeAttribute("hidden"))
+    .then(document.getElementById("admission-form").reset())
+    .then( document.getElementById("name").classList.remove('is-valid'))
+    .then( document.getElementById("phone").classList.remove('is-valid'))
+    .then( document.getElementById("grade").classList.remove('is-valid'))
     .finally(setTimeout(function(){
       document.getElementById("res_success").setAttribute("hidden","hidden");
       document.getElementById("res_error").setAttribute("hidden","hidden");
@@ -53,7 +60,7 @@ function Send_Team_Data(){
   setTimeout(function () {
     document.getElementById("teachbtn").removeAttribute('disabled');
   }, 3000);
-  document.getElementById("teacher-form").reset();
+ 
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => document.getElementById("res_suc").removeAttribute("hidden"))
     .catch(error => document.getElementById("res_err").removeAttribute("hidden"))
@@ -76,8 +83,12 @@ function Send_Team_Data(){
       mimeType: file.type,
     });
     fetch(`${url}?${qs}`, {method: "POST", body: JSON.stringify([...new Int8Array(f.target.result)])})
-      .then(res => console.log("Succes"))
-      .catch(res => console.log("Error"));
+      .then(res =>document.getElementById("teacher-form").reset())
+      .then( document.getElementById("phone").classList.remove('is-valid'))
+      .then( document.getElementById("email").classList.remove('is-valid'))
+      .then( document.getElementById("ExperienceInYears").classList.remove('is-valid'))
+      .catch(res => console.log("Error"))
+      .finally(document)
   };
 }
 
